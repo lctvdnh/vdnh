f46a1852947afddd7de30f7214b3655600308013
root <root@geotree.ru>
2022-11-06 14:08:06 +0300

    commit

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 		<a href="./map.php?all=events">Показать все события</a><br>
 		<a href="./map.php?heatmap">Тепловая карта по количеству посетителей за 2022 год</a><br>
 		<a href="https://api.mapbox.com/styles/v1/gali19/cla2plckp00p815mg93alepsd.html?title=view&access_token=pk.eyJ1IjoiZ2FsaTE5IiwiYSI6ImNsOXprNHJ0MTBiZnozdnI4bHE5MDVyYmoifQ.Salw0z9ytO43tJIBCWtmsA&zoomwheel=true&fresh=true#14.28/55.83398/37.6251">Тепловая карта. Маршрутная нагрузка</a><br>
-		<a href="https://api.mapbox.com/styles/v1/gali19/cla4fkxdn004s15nxwsw4cir6.html?title=view&access_token=pk.eyJ1IjoiZ2FsaTE5IiwiYSI6ImNsOXprNHJ0MTBiZnozdnI4bHE5MDVyYmoifQ.Salw0z9ytO43tJIBCWtmsA&zoomwheel=true&fresh=true#14.66/55.836/37.62467">Карта прогноза посещаемости</a><br>
+		<a href="https://api.mapbox.com/styles/v1/gali19/cla4fkxdn004s15nxwsw4cir6.html?title=view&access_token=pk.eyJ1IjoiZ2FsaTE5IiwiYSI6ImNsOXprNHJ0MTBiZnozdnI4bHE5MDVyYmoifQ.Salw0z9ytO43tJIBCWtmsA&zoomwheel=true&fresh=true#14.66/55.836/37.62467">Карта прогноза посещаемости</a> (<a href="./data/точки для mapbox.geojson">геоточки</a>, <a href="./data/Прогноз_посещаемости_по_объектам.xlsx">таблица Excel</a>)<br>
 		<a href="https://forms.yandex.ru/u/6366960c693872000dbb186c/">Обратная связь</a><br>
 		<a href="http://project6309404.tilda.ws/vdnh">Демо версия кабинета администрации (загрузка типовых маршрутов)</a><br>
 	</body>
