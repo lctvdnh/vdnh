0232321ae17309d389727d9482ee8bb2cd746f2b
root <root@geotree.ru>
2022-11-06 21:43:18 +0300

    commit

--- index.php
+++ index.php
@@ -15,6 +15,8 @@
 		<a href="https://api.mapbox.com/styles/v1/gali19/cla2plckp00p815mg93alepsd.html?title=view&access_token=pk.eyJ1IjoiZ2FsaTE5IiwiYSI6ImNsOXprNHJ0MTBiZnozdnI4bHE5MDVyYmoifQ.Salw0z9ytO43tJIBCWtmsA&zoomwheel=true&fresh=true#14.28/55.83398/37.6251">Тепловая карта. Маршрутная нагрузка</a><br>
 		<a href="https://api.mapbox.com/styles/v1/gali19/cla4fkxdn004s15nxwsw4cir6.html?title=view&access_token=pk.eyJ1IjoiZ2FsaTE5IiwiYSI6ImNsOXprNHJ0MTBiZnozdnI4bHE5MDVyYmoifQ.Salw0z9ytO43tJIBCWtmsA&zoomwheel=true&fresh=true#14.66/55.836/37.62467">Карта прогноза посещаемости</a><br>
 		<a href="https://forms.yandex.ru/u/6366960c693872000dbb186c/">Обратная связь</a><br>
+		<a href="https://drive.google.com/drive/folders/1qeFJw_6wlcdYilF8RK9LRCWwvFG0YpTx?usp=sharing">AR навигация (пакет установщика для андроид)</a><br>
+		<a href="https://disk.yandex.ru/i/sfeIJpT-BaKfTA">AR интеграция с существующей программой лояльности</a><br>
 		<a href="http://project6309404.tilda.ws/vdnh">Демо версия кабинета администрации (загрузка типовых маршрутов)</a><br>
 	</body>
 </html>
